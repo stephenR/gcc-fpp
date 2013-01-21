b9ffa6c26b65bb897134c34e4d2c497c5fffa846
Stephen Röttger <stephen.roettger@zero-entropy.de>
2013-01-21 20:28:05 +0100

    fpp_protect and fpp_verify proof of concept

--- libfpprotect/libfpprotect.c
+++ libfpprotect/libfpprotect.c
@@ -5,11 +5,20 @@
 #include <unistd.h>
 #include <link.h>
 
+typedef void (*function_pointer_t)();
+
+/* TODO: change type to a pointer type
+ * Somehow, gcc doesn't put the var in .rodata if it's a void * */
 static const long long __fpp_reg = 0;
+
+#define FPP_REGION ((char *) __fpp_reg)
 
 static const size_t mmap_initial_size = 4096;
 
 static int dl_iterate_phdr_callback (struct dl_phdr_info *info, size_t x, void *data);
+static int initialize_jumppad_region();
+static void lock();
+static void unlock();
 
 static void fpprotect_init() __attribute__ ((constructor));
 static void fpprotect_init()
@@ -17,53 +26,92 @@ static void fpprotect_init()
 	dl_iterate_phdr (dl_iterate_phdr_callback, NULL);
 }
 
-static int
+	static int
 dl_iterate_phdr_callback (struct dl_phdr_info *info, size_t x, void *data)
 {
-  int j;
+	int j;
 
-  for (j = 0; j < info->dlpi_phnum; j++)
-    {
-      ElfW(Addr) relocated_start_addr =
-          info->dlpi_addr + info->dlpi_phdr[j].p_vaddr;
-      ElfW(Addr) unrelocated_start_addr = info->dlpi_phdr[j].p_vaddr;
-      ElfW(Addr) start_addr = relocated_start_addr + unrelocated_start_addr;
-      ElfW(Word) size_in_memory = info->dlpi_phdr[j].p_memsz;
+	for (j = 0; j < info->dlpi_phnum; j++)
+	{
+		ElfW(Addr) relocated_start_addr =
+			info->dlpi_addr + info->dlpi_phdr[j].p_vaddr;
+		ElfW(Addr) unrelocated_start_addr = info->dlpi_phdr[j].p_vaddr;
+		ElfW(Addr) start_addr = relocated_start_addr + unrelocated_start_addr;
+		ElfW(Word) size_in_memory = info->dlpi_phdr[j].p_memsz;
 
-      if ((long long) &__fpp_reg < start_addr || (long long) &__fpp_reg >= start_addr + size_in_memory)
-	      continue;
+		if ((long long) &__fpp_reg < start_addr || (long long) &__fpp_reg >= start_addr + size_in_memory)
+			continue;
 
-      ElfW(Word) saved_flags = info->dlpi_phdr[j].p_flags;
+		ElfW(Word) saved_flags = info->dlpi_phdr[j].p_flags;
 
-      ElfW(Addr) mp_low = relocated_start_addr & ~(sysconf(_SC_PAGESIZE) - 1);
-      size_t mp_size = relocated_start_addr + size_in_memory - mp_low - 1;
+		ElfW(Addr) mp_low = relocated_start_addr & ~(sysconf(_SC_PAGESIZE) - 1);
+		size_t mp_size = relocated_start_addr + size_in_memory - mp_low - 1;
 
-      if (mprotect((void *)mp_low, mp_size, PROT_READ|PROT_WRITE|PROT_EXEC) == -1)
-        {
-          perror("mprotect");
-          return -1;
-        }
+		if (mprotect((void *)mp_low, mp_size, PROT_READ|PROT_WRITE|PROT_EXEC) == -1)
+		{
+			perror("mprotect");
+			return -1;
+		}
 
-      *((void **)&__fpp_reg) = mmap(NULL, mmap_initial_size, PROT_READ | PROT_EXEC, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
-      if((void *)__fpp_reg == MAP_FAILED)
-        {
-        perror("mprotect");
-        return -2;
+		if(initialize_jumppad_region())
+		{
+			perror("mprotect");
+			return -2;
+		}
+
+		if (mprotect((void *)mp_low, mp_size, saved_flags) == -1)
+		{
+			perror("mprotect");
+			return -3;
+		}
+
+		return 1;
 	}
-
-      if (mprotect((void *)mp_low, mp_size, saved_flags) == -1)
-        {
-          perror("mprotect");
-	  return -3;
-        }
-
-      return 1;
-    }
-  return 0;
+	return 0;
 }
 
-void __fpp_foo(void * x)
+static int initialize_jumppad_region()
 {
-	printf("x=%p, __fpp_reg=%p\n", x, __fpp_reg);
+	*((void **)&__fpp_reg) = mmap(NULL, mmap_initial_size, PROT_READ | PROT_EXEC, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+	if((void *)__fpp_reg == MAP_FAILED)
+	{
+		return -1;
+	}
+	return 0;
+}
+
+void *__fpp_protect(void *p)
+{
+	unlock();
+        *((long long *) FPP_REGION) = 0x25ff;
+	*((long long *)(FPP_REGION+6)) = p;
+	lock();
+	return FPP_REGION;
+}
+
+function_pointer_t __fpp_verify(void *p)
+{
+	if(p < FPP_REGION || p >= FPP_REGION + mmap_initial_size) {
+		puts("failed");
+		_exit(1);
+	}
+	return p;
+}
+
+static void lock()
+{
+	if (mprotect(FPP_REGION, mmap_initial_size, PROT_READ|PROT_EXEC) == -1)
+	{
+		perror("mprotect");
+		_exit(1);
+	}
+}
+static void unlock()
+{
+	if (mprotect(FPP_REGION, mmap_initial_size, PROT_READ|PROT_WRITE) == -1)
+	{
+		perror("mprotect");
+		_exit(1);
+	}
 }
 
